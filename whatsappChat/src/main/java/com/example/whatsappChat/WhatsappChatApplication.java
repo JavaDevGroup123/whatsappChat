939bf48de38b306da081bed6c42bf3854c7db8a1
JavaDevGroup123 <swiminjavaocean@gmail.com>
2023-07-25 17:16:27 +0530

    Initial commit

---
+++ whatsappChat/src/main/java/com/example/whatsappChat/WhatsappChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.whatsappChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappChatApplication.class, args);
+	}
+
+}
